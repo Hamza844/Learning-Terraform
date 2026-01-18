7c53d4d1b91e985188bdd8d13895b40b99e652b6
hamza844 <jamhamza844@gmail.com>
2026-01-19 01:06:30 +0500

    Initial Terraform setup

---
+++ resource.tf
@@ -0,0 +1,4 @@
+resource local_file myfile {
+    filename = hamza.txt
+    content = "Hello my name is Hamza and I am DevOps Engineer"
+}
---
+++ s3.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "learning24" {
+
+    bucket = "myweb3"
+  
+}
+resource "aws_instance" "hamzaweb3" {
+    
+}
